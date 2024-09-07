777792ab47f60cdc6c79548c839120b751d318ac
Immaculate Busisiwe Mchunu <boocymchunu@gmail.com>
2024-09-07 17:16:39 +0200

    added the AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-function generatePoem(event) {
-    event.preventDefault();
-
-    alert("Generating Poem");
-
-    let poemElement= document.querySelector ("poem");
-
-    new Typewriter("poem", {
-  strings: ["Pretty women wonder where my secret lies. "],
+ 
+function displayPoem(responce) {
+ 
+    console.log("poem generated");
+ new Typewriter("poem", {
+  strings: responce.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "",
 });
 }
+function generatePoem(event) {
+    event.preventDefault();
+}
 
+    let instructionsInput = document.querySelector("user-instructions");
+    let apiKey = "b1a0o1ft9108d6b76f71c2fa34c10e16 ";
+    let prompt = 'user instructions:generate an english poem about women ${instructionsInput.value}';
+    let context = "you are an english poem writer and love to write poems. your mission is to generate a 4 line in basic HTML. make sure to follow the user instructions below.";
+    let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}';
+
+    console.log("generating poem");
+    console.log('prompt: ${prompt}');
+    console.log('context: ${context}');
+
+    axios.get(apiURL).then(displayPoem);
+
+    alert("Generating Poem");
+
+    let poemElement= document.querySelector ("poem");
 
 let poemFormElement = document.querySelector("poem-genegator-form")
 poemFormElement.addEventListener("submit", generatePoem);
